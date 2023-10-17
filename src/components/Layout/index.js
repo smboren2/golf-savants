4585eeda98f7f525f8b34a8018bac7941bd718de
Sean Boren <smboren2@gmail.com>
2023-10-17 12:05:12 -0500

    messing with navbar, sloppy, but will fix

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,20 +1,29 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import logo from "../../images/logo.png"
 import { StaticQuery, graphql, Link } from 'gatsby'
 import {
     container,
-    heading,
-    navLinks,
+    navbar,
+    navbarLeft,
+    navbarRight,
     navLinkItem,
     navLinkText,
-    navbar,
-    navLeft
+    heading,
+    dropdown,
+    dropbtn,
+    dropdownContent
   } from './layout.module.css'
 
 
-  
+
 
 const Layout = ({ pageTitle, children }) => {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+
     return (
         <StaticQuery
             query = {graphql`
@@ -34,25 +43,41 @@ const Layout = ({ pageTitle, children }) => {
             render={data => (
                 <div className={container}>
                     <nav className={navbar}>
-                        <div className={navLeft}>
-                            <Link to="/">
-                                <img src={logo} alt="Golf Savants" width="400" />
-                            </Link>
-                        </div>
-                        <div>
-                            <ul className={navLinks}>
-                                {data.site.siteMetadata.menuLinks.map(link => (
-                                    <li className={navLinkItem}>
-                                        <Link to={link.link} className={navLinkText}>{link.name}</Link>
-                                        </li>
-                                    ))}
-                            </ul>
-                        </div>
+                      <div className={navbarLeft}>
+                        <Link to="/">
+                          <img src={logo} alt="Golf Savants"/>
+                        </Link>
+                      </div>
+                      <div>
+                        <ul className={navbarRight}>
+                            {data.site.siteMetadata.menuLinks.map(link => (
+                          <li className={navLinkItem}>
+                            <Link to={link.link} className={navLinkText}>{link.name}</Link>
+                          </li>
+                          ))}
+                          <div className={dropdown}>
+                            <button onClick={toggleDropdown} className={dropbtn}>
+                                Tools
+                            </button>
+                            {isDropdownOpen && (
+                            <div className={dropdownContent}>
+                                <a href="https://www.facebook.com">Facebook</a>
+                                <a href="https://www.linkedin.com">LinkedIn</a>
+                                <a href="https://www.twitter.com">Twitter</a>
+                            </div>
+                            )}
+                          </div>
+                        </ul>
+                      </div>
+                      
                     </nav>
                     <main>
                        <h1 className={heading}>{pageTitle}</h1>
                         {children}
                     </main>
+                    <footer>
+                        <p>Golf Savants {new Date().getFullYear()}</p>
+                    </footer>
                 </div>
                     )
                 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout/'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout>
+    <Layout pageTitle={"Welcome!"}>
       <p>Welcome to Golf Savants, a golf analytics website focused on giving users the tools necessary to 
         become profitable DFS players as well as +EV bettors. We are here to help you make smarter decisions in daily fantasy sports and golf betting. 
         Whether you're a seasoned golf aficionado or just getting started, our comprehensive suite of cutting-edge data tools 
