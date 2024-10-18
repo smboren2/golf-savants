695d0b2c42afc3f87926145e263dbfe51a01069d
Sean Boren <smboren2@gmail.com>
2024-10-18 08:09:07 -0500

    updating landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-purple-800 mb-4">Welcome to Golf Savants</h1>
-      <p className="text-lg text-gray-700">This is your new Next.js homepage, styled with Tailwind CSS.</p>
+      <p className="text-lg text-gray-700">We're currently under construction. More to come...</p>
     </div>
   )
 }
